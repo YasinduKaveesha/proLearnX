7f07396e24fab78d9b7e465746ce74c590638de9
your_name <your_email@example.com>
2025-04-20 01:14:23 +0530

    Initial commit for frontend and backend in monorepo

---
+++ backend/src/main/java/com/proLearnX/proLearnX/ProLearnXApplication.java
@@ -0,0 +1,13 @@
+package com.proLearnX.proLearnX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProLearnXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProLearnXApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/proLearnX/proLearnX/ProLearnXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proLearnX.proLearnX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProLearnXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
